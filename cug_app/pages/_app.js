43b2114d0ffd5a8f7f3728a8eb975e28fa59a902
ChinmayDaliNuzpapr <chinmay.d@nuzpapr.com>
2021-05-31 19:22:32 +0530

    The navbar component created

--- cug_app/pages/_app.js
+++ cug_app/pages/_app.js
@@ -1,7 +1,13 @@
 import "../styles/globals.css";
 // import "tailwindcss/tailwind.css";
+import Navbar from "../components/Nav.js";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
