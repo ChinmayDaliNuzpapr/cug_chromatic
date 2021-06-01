83986c1197d0a9d0e79c736d7bd15a1e0cc3f76b
bhupeshnuzpapr <bhupesh.b@nuzpapr.com>
2021-06-01 17:57:38 +0530

    home components

---
+++ cug_app/stories/Home/Button/Button.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Button from './Button';
+
+//default export
+export default {
+	title: 'Home/Button', //
+	component: Button,
+};
+
+//named export for variations
+export const Component_Button = () => <Button />;
--- cug_app/stories/Home/Screen1_Footer/Footer.js
+++ cug_app/stories/Home/Screen1_Footer/Footer.js
@@ -1,36 +1,23 @@
 import React from 'react';
+import '../../../styles/globals.css';
+import Footer_Feature from './Footer_Feature';
 
 const Footer = () => {
 	return (
 		<div className='bg-blue-200 p-2'>
-			<h1 className='text-center	text-align: center text-4xl font-size: 2.25rem+-* m-1.5	margin: 0.375rem'>
-				Title 1
-			</h1>
-			<p className='text-center text-align: center m-4 margin: 1rem'>
+			<p className='montserrat-30 text-center text-align: center text-4xl font-size: 2.25rem+-* m-1.5 margin: 0.375rem'>
+				<span>Title 1</span>
+			</p>
+			<p className=' mb-8	margin-bottom: 2rem montserrat-18 text-center text-align: center m-4 margin: 1rem'>
 				Here are the few categories that you can discuss, ask, answer, recommend
 				here
 			</p>
-			<div className='flex justify-around justify-content: space-around m-8'>
-				<div>
-					<img src='/favicon.io' />
-					<p>Category 1</p>
-				</div>
-				<div>
-					<img src='/favicon.io' />
-					<p>Category 2</p>
-				</div>
-				<div>
-					<img src='/favicon.io' />
-					<p>Category 3</p>
-				</div>
-				<div>
-					<img src='/favicon.io' />
-					<p>Category 4</p>
-				</div>
-				<div>
-					<img src='/favicon.io' />
-					<p>Category 5</p>
-				</div>
+			<div className=' montserrat-12 text-center text-align: center flex justify-around justify-content: space-around '>
+				<Footer_Feature />
+				<Footer_Feature />
+				<Footer_Feature />
+				<Footer_Feature />
+				<Footer_Feature />
 			</div>
 		</div>
 	);
