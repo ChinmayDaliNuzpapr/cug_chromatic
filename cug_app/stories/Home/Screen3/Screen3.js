b2f48dbedfb387d73aa4b9677430af4aa664b777
ChinmayDaliNuzpapr <chinmay.d@nuzpapr.com>
2021-06-01 20:11:26 +0530

    updating screens and title_2_component

--- cug_app/stories/Home/Screen3/Screen3.js
+++ cug_app/stories/Home/Screen3/Screen3.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import Screen3_Feature from './Screen3_Feature';
-import '../../../styles/globals.css';
-import image from '../../../public/screen3.jpg';
+import React from "react";
+import Screen3_Feature from "./Screen3_Feature";
+import "../../../styles/globals.css";
+import image from "../../../public/GetImage.gif";
 
 const Screen3 = () => {
-	return (
-		<div className='bg-blue-200 p-5'>
-			<div className='mb-8'>
-				<p className=' text-blue-900 montserrat-30 text-center  text-4xl font-size: 2.25rem+-* m-1.5 margin: 0.375rem'>
-					<span>Title 3</span>
-				</p>
-				<p className='montserrat-18 text-center text-align: center m-4 margin: 1rem'>
-					2 lines of text is to be placed here
-				</p>
-			</div>
-			<div>
-				<div className=' flex justify-around'>
-					<div>
-						<img className='h-300 m-auto margin: auto' src={image} />
-					</div>
-					<div className='grid grid-cols-1 sm:grid-cols-2 gap-10 '>
-						<Screen3_Feature />
-						<Screen3_Feature />
-						<Screen3_Feature />
-						<Screen3_Feature />
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className="bg-blue-200 p-5">
+      <div className="mb-8">
+        <p className=" text-blue-900 montserrat-30 text-center  text-4xl font-size: 2.25rem+-* m-1.5 margin: 0.375rem">
+          <span>Title 3</span>
+        </p>
+        <p className="montserrat-18 text-center text-align: center m-4 margin: 1rem">
+          2 lines of text is to be placed here
+        </p>
+      </div>
+      <div>
+        <div className=" flex justify-around">
+          <div>
+            <img className="h-300 m-auto margin: auto" src={image} />
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 ">
+            <Screen3_Feature />
+            <Screen3_Feature />
+            <Screen3_Feature />
+            <Screen3_Feature />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Screen3;
--- cug_app/stories/Home/Title_two_comp/title2.js
+++ cug_app/stories/Home/Title_two_comp/title2.js
@@ -1,61 +1,30 @@
 import React from "react";
 import image from "../../../public/GetImage.gif";
 import category_image from "../../../public/category_placeholder.svg";
+import Screen3_Feature from "../Screen3/Screen3_Feature";
 const title2 = () => {
   return (
     <div className="title2_css ">
+      <p className="montserrat-30 text-center text-align: center text-4xl font-size: 2.25rem+-* m-1.5 margin: 0.375rem">
+        <span>Title 1</span>
+      </p>
+      <p className=" mb-8	margin-bottom: 2rem montserrat-18 text-center text-align: center m-4 margin: 1rem">
+        Here are the few categories that you can discuss, ask, answer, recommend
+        here
+      </p>
       <div className="grid grid-cols-2 gap-4">
         {/* Column-1 */}
         <div className="flex flex-col p-2 justify-end">
           {/* Single sub-coloumn */}
           <div className="p-10">
             {/* Row-1 */}
-            <div className="flex flex-row justify-center space-x-10">
-              {/* Two cards will be side-by-side */}
-              {/* CARD-1 */}
-              <div
-                id="app"
-                className="bg-white w-full h-60 rounded flex items-center card"
-              >
-                <img
-                  // className="w-1/2 rounded-l-sm"
-                  width="90"
-                  height="60"
-                  src={category_image}
-                  alt="Room Image"
-                />
-                <div className="w-full flex flex-col">
-                  <div className="p-4 pb-0 flex-1">
-                    <span className="text-4xl text-grey-darkest">Title-1</span>
-                    <br />
-                    <h3 className="font-light mb-1 text-grey-darkest">
-                      sub-title
-                    </h3>
-                  </div>
-                </div>
-              </div>
-              {/* CARD-2 */}
-              <div
-                id="app"
-                className="bg-white w-full h-60 rounded flex items-center card"
-              >
-                <img
-                  // className="w-1/2 rounded-l-sm"
-                  width="90"
-                  height="60"
-                  src={category_image}
-                  alt="Room Image"
-                />
-                <div className="w-full flex flex-col">
-                  <div className="p-4 pb-0 flex-1">
-                    <span className="text-4xl text-grey-darkest">Title-1</span>
-                    <br />
-                    <h3 className="font-light mb-1 text-grey-darkest">
-                      sub-title
-                    </h3>
-                  </div>
-                </div>
-              </div>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-10 ">
+              <Screen3_Feature />
+              <Screen3_Feature />
+              <Screen3_Feature />
+              <Screen3_Feature />
+              <Screen3_Feature />
+              <Screen3_Feature />
             </div>
           </div>
         </div>
