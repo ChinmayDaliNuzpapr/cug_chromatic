7cbd3ee4a44be811779f5c59eae1e1ca9590b9c2
ChinmayDaliNuzpapr <chinmay.d@nuzpapr.com>
2021-06-02 15:47:12 +0530

    The subscribe component is added

---
+++ cug_app/stories/Home/SubscribeComponent/Subscribe.stories.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Subscribe from "../../../components/Home/subscribe_component/subscribe";
+
+//default export
+export default {
+  title: "Home/Subscribe", //
+  component: Subscribe,
+};
+
+//named export for variations
+export const Component_Subscribe = () => <Subscribe />;
