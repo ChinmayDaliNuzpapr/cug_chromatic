ac149c657c6a8b38a01216030f0180e6f7d05a9a
ChinmayDaliNuzpapr <chinmay.d@nuzpapr.com>
2021-06-15 17:31:59 +0530

    setting up the state-management for the new layout

--- cug_app/pages/questions/index.js
+++ cug_app/pages/questions/index.js
@@ -7,57 +7,92 @@
 // Incorrect thought process👇
  🚩 we will use useCallback but one failure would be this will query uncessarily the same 10 question over and over
 */
-import React, { useState, useEffect, useContext } from "react";
+import React, { useState, useEffect, useContext, createContext } from "react";
 import BoxComponent from "../../components/question/BoxComponent";
 import QuestionCard from "../../components/QuestionCard";
+import CreateQuestionForm from "../../components/question/article/CreateQuestionForm";
 import QuestionComponent from "../../components/QuestionComponent";
 import Sidebar from "../../components/Workflow/Sidebar";
 import { Trending, ExtraTrendingBox } from "../../components/Workflow/Trending";
 import { useRouter } from "next/router";
 import { UserContext } from "../../components/Layout";
+import styles from "./sidebar.module.css";
+import Layout from "../../components/question/Layout";
+
+export const StropContext = createContext(null);
 
 const index = () => {
-  // const [category, setCategory] = useState("home");
-  const category = [
-    "home",
-    "category 1",
-    "category 2",
-    "category 3",
-    "category 4",
-    "category 5",
-    "category 6",
-    "category 7",
-    "category 8",
-    "category 9",
-    "category 10",
-    "category 11",
-    "category 12",
-    "category 13",
-  ];
+  const [category, setCategory] = useState({
+    current_category: "home",
+    category_list: [
+      "home",
+      "category 1",
+      "category 2",
+      "category 3",
+      "category 4",
+      "category 5",
+      "category 6",
+      "category 7",
+      "category 8",
+      "category 9",
+      "category 10",
+      "category 11",
+      "category 12",
+      "category 13",
+    ],
+  });
+  const [selectedQuestion, setselectedQuestion] = useState(false);
+  const [askQuestion, setAskQuestion] = useState(false);
+  console.log("THE LOCAL STATE", category, selectedQuestion, askQuestion);
   const { authenticated } = useContext(UserContext);
   const router = useRouter();
+  /* [📌] The below code is commented for developement purposes.
   // The below effect will only run if the user has logged out
-  // useEffect(() => {
-  //   if (authenticated === null) {
-  //     router.push("/");
-  //   }
-  // }, [authenticated]);
+   useEffect(() => {
+     if (authenticated === null) {
+       router.push("/");
+     }
+   }, [authenticated]);
 
-  // const [];
+  */
   return (
     <div className="container mx-auto">
       <div className="flex flex-col my-16">
         <div className="flex flex-row">
-          <div className=" md:w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
-            {/* flex-grow */}
-            <Sidebar category={category} />
+          <div className="hidden md:block">
+            <div
+              id="sidebar_div"
+              className="md:w-full md:w-1/2 lg:w-1/4 px-2 mb-4"
+            >
+              <Sidebar category={category} category_func={setCategory} />
+            </div>
           </div>
-          <div className="w-full lg:w-1/2 px-2">
-            <BoxComponent />
-            <QuestionComponent />
+          {/* <Layout /> */}
+          <div className="flex-grow w-full lg:w-1/2 px-2">
+            {selectedQuestion === false ? (
+              <>
+                <BoxComponent />
+                <QuestionComponent
+                  setselectedQuestion={setselectedQuestion}
+                  askQuestion_func={setAskQuestion}
+                />
+              </>
+            ) : selectedQuestion === true ? (
+              <>
+                <DetailQuestionComponent />
+              </>
+            ) : (
+              <></>
+            )}
+            {askQuestion === true ? (
+              <>
+                <CreateQuestionForm />
+              </>
+            ) : (
+              <></>
+            )}
           </div>
-          <div className=" w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
-            {/* flex-grow sm:hidden */}
+          <div className="sr-only w-full md:w-1/2 lg:w-1/4 px-2 mb-4">
             <ExtraTrendingBox />
           </div>
         </div>
@@ -68,7 +103,4 @@ const index = () => {
 
 export default index;
 
-/*
-
-
-*/
+const DetailQuestionComponent = <div>DETAILS OF A QUESTION</div>;
