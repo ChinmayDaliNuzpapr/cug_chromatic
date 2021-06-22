aa86b8a69a3b10af8f8d0e738281367768018706
ChinmayDaliNuzpapr <chinmay.d@nuzpapr.com>
2021-06-22 12:31:29 +0530

    The box-component now has the group-name

--- cug_app/components/question/BoxComponent.jsx
+++ cug_app/components/question/BoxComponent.jsx
@@ -8,18 +8,17 @@ const BoxComponent = () => {
         <main className=" flex justify-center">
           <div className="flex flex-col md:min-w-full p-3 space-y-5 rounded-xl bg-white ">
             <section className="text-sm font-thin text-orange-400">
-              SOME RANDOM STRING
+              {/* SOME RANDOM STRING */}
             </section>
-            <section className="text-3xl font-bold">
-              {/* Welcome to {fetchedData && fetchedData.group_data.Group_name} */}
+            <section className="text-3xl text-center font-bold">
+              Welcome to{" "}
+              {fetchedData &&
+                fetchedData.group_data &&
+                fetchedData.group_data.Group_name}
             </section>
             <section className="font-normal text-md text-gray-700">
-              Have Fun <br />
-              {/* authentication link sent on your email */}
+              {/* Have Fun <br /> */}
             </section>
-            {/* <section className="font-bold text-lg text-blue-900">
-              Nuzpapr-Technolabs
-            </section> */}
           </div>
         </main>
       </div>
