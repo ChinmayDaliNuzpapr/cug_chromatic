69cc0c153a8df1c241ef1b672b4544db77491f96
ChinmayDaliNuzpapr <chinmay.d@nuzpapr.com>
2021-06-23 15:15:19 +0530

    search part completed

--- cug_app/components/question/SearchBar.jsx
+++ cug_app/components/question/SearchBar.jsx
@@ -1,14 +1,84 @@
 import React from "react";
-import Head from "next/head";
+import axios from "axios";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import { MainDataContext } from "../Layout";
 const SearchBar = () => {
+  const [searchtext, setSearchText] = React.useState("");
+  const { fetchedData, setFetchedData, setLoading } = React.useContext(
+    MainDataContext
+  );
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("THE VALUE ", searchtext);
+    if (searchtext[0] === "#") {
+      console.log("THE SUBSTRING", searchtext.substring(1));
+      axios
+        .post(
+          `http://localhost:3000/api/search/question`,
+          {
+            search: searchtext.substring(1),
+            searchBy: "tag",
+            scope: "company", // set this as the current scope
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log("THE RES Data COMPANY/SCOPE", res);
+          setFetchedData({ ...fetchedData, questions: res.data.question });
+          // setLoading(false);
+        })
+        .catch((err) => console.log("😈😈😈😈", err));
+    } else {
+      axios
+        .post(
+          `http://localhost:3000/api/search/question`,
+          {
+            search: searchtext,
+            searchBy: "title",
+            scope: "company",
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("jwt_token")}`,
+            },
+          }
+        )
+        .then((res) => {
+          console.log("THE RES Data COMPANY/SCOPE", res);
+          setFetchedData({ ...fetchedData, questions: res.data.question });
+          setLoading(false);
+        })
+        .catch((err) => console.log("😈😈😈😈", err));
+    }
+  };
+
   return (
     <>
-      <form id="searchbar" action="">
-        <input id="searchbar_input" type="search" />
+      {/* <Formik> */}
+      <form id="searchbar" onSubmit={onFormSubmit}>
+        <input
+          id="searchbar_input"
+          type="search"
+          name="search"
+          value={searchtext}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
         <i id="search_icon" className="fa fa-search"></i>
       </form>
+      {/* </Formik> */}
     </>
   );
 };
 
 export default SearchBar;
+
+// const SearchTextField= ({props}) => {
+//   return(
+
+//     )
+// }
